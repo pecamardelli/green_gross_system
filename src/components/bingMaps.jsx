5f91b9e10611db259ab3a69a14088147ffcc488e
Pablo Camardelli <pecamardelli@gmail.com>
2020-12-12 15:16:18 -0300

    Add pushpin example

--- src/components/bingMaps.jsx
+++ src/components/bingMaps.jsx
@@ -2,26 +2,42 @@ import React, { useEffect } from 'react';
 
 function BingMaps(props) {
   const apiKey = "Aj6-7A0g8ZfYerfMQLQVFt3DvU--RyMpDC8u1g2KV_CFP4plypNxDSWei9wbEpbK";
+  let map;
   
   useEffect(() => {
     const load = setInterval(() => {
       if (window.Microsoft) {
-        new window.Microsoft.Maps.Map('#myMap', {
+        map = new window.Microsoft.Maps.Map('#myMap', {
         credentials: apiKey,
-        center: new window.Microsoft.Maps.Location(51.50632, -0.12714),
+        center: new window.Microsoft.Maps.Location(43.254406, -79.867308),
         mapTypeId: window.Microsoft.Maps.MapTypeId.aerial,
-            zoom: 10
+            zoom: 15
         });
+
+        const center = map.getCenter();
+
+        //Create custom Pushpin
+        const pin = new window.Microsoft.Maps.Pushpin(center, {
+            title: 'Green Gross',
+            subTitle: 'Main Branch',
+            text: '1'
+        });
+
+        //Add the pushpin to the map
+        map.entities.push(pin);
       }
       if (window.Microsoft) window.clearInterval(load);
     }, 1000);
-    
   });
 
   return (
-    <div className="card-transparent bing-maps">
-      <div className="card-body">
-        <div id="myMap"></div>
+    <div className="card-transparent bing-maps text-white">
+      <div className="card-body row">
+        <div className="col-sm-6" id="myMap"></div>
+        <div className="col-sm-6">
+          <h4>Please, enter your address.</h4>
+          <input className="form-control" type="text" />
+        </div>
       </div>
     </div>
   );
